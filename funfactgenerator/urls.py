6559a0b4961924872f48c863e921f612ed736fb2
azinuddinazlani <azinuddinazlani@gmail.com>
2025-05-04 17:25:06 +0800

    Fixed mistake in funfactgenerator folder

--- funfactgenerator/urls.py
+++ funfactgenerator/urls.py
@@ -14,10 +14,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path
-from . import views
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('api/random-fact/', views.get_random_fact, name='random_fact'),
+    path('admin/', admin.site.urls),
+    path('', include('facts.urls')),
 ]
